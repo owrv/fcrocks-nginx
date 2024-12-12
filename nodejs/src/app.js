d5857668d1c785ef4321432c7b859f9051070ccf
Wellersom Richardsom Voigt <wellersom.wrv@gmail.com>
2024-12-12 13:48:36 -0300

    componente routes é usado como roteador pelo express

--- nodejs/src/app.js
+++ nodejs/src/app.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import routes from './routes.js';
 class App {
     constructor() {
         this.server = express();
+        this.routes();
     }
+    routes() {
+        this.server.use(routes);
     }
 }
 export default new App().server;
