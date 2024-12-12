67442e0a395e945f2ad32255a7800940c057d0b0
Wellersom Richardsom Voigt <wellersom.wrv@gmail.com>
2024-12-12 14:03:17 -0300

    Ao iniciar a aplicação, lista de nomes é atualizada, deixando nomes em memoria prontos para serem usados

--- nodejs/src/app.js
+++ nodejs/src/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import routes from './routes.js';
+import GetNames from './app/job/getNames.js';
+
 class App {
     constructor() {
         this.server = express();
         this.server.set('view engine', 'ejs');
         this.server.set('views', './views');
         this.routes();
+        GetNames.handle();
     }
     routes() {
         this.server.use(routes);
