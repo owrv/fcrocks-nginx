450f10d7bea7c3fcf262a293383e0635a02ef7e5
Wellersom Richardsom Voigt <wellersom.wrv@gmail.com>
2024-12-12 14:00:47 -0300

    Inclui o primeiro nome da lista de nomes no banco de dados e retorna uma pagina com nos nomes da base de dados

--- nodejs/src/app/controllers/people.js
+++ nodejs/src/app/controllers/people.js
@@ -1,5 +1,14 @@
+import getNames from '../job/getNames.js';
+import queries from '../../database/queries.js';
 class PeopleController {
     async store(req, res) {
+        const name = getNames.get();
+        queries.myInsertQuery(name[0]);
+        getNames.removeFirstName();
+        const result = queries.mySelectQuery();
+        result.then((peoples)=>{
+            res.render('pagina', { dados: peoples });
+        });
     }
 }
 export default new PeopleController();
