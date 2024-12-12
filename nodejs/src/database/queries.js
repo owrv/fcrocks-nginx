aa6587002022a69278d544868ba67b0e716b61cf
Wellersom Richardsom Voigt <wellersom.wrv@gmail.com>
2024-12-12 14:02:17 -0300

    operação de inserir e buscar nome

--- nodejs/src/database/queries.js
+++ nodejs/src/database/queries.js
@@ -1,3 +1,5 @@
+import mysql from 'mysql2'
+
 class Queries {
     constructor() {
         this.connection = mysql.createConnection({
@@ -8,4 +10,24 @@ class Queries {
             insecureAuth: true
         });
     }
+    mySelectQuery () {
+        return new Promise((resolve, reject) => {
+            this.connection.connect();
+            this.connection.query('SELECT cod_people, des_name FROM peoples', function (error, result, fields) {
+                if (error) {
+                    console.log(`Erro ao tentar abrir uma nova conexão: ${err}`)
+                } else {
+                    return resolve(result);
+                }
+            });
+        });
+    }
+    myInsertQuery(nameParam) {
+        this.connection.connect();
+        this.connection.query('INSERT INTO peoples (des_name) VALUES (?)', [nameParam],(error, results) => {
+            if (error)
+                console.log('Error in query insert: ', error)
+        });
+    };
+};
 export default new Queries;
