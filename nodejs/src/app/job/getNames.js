8affa15aff3f0ad3cd228c192454cf1739e82686
Wellersom Richardsom Voigt <wellersom.wrv@gmail.com>
2024-12-12 13:57:10 -0300

    resolve a requisição de nomes

--- nodejs/src/app/job/getNames.js
+++ nodejs/src/app/job/getNames.js
@@ -1,4 +1,12 @@
 class GetNames {
     _listNames = [];
+    async handle() {
+        const resServicoDados = await fetch('https://servicodados.ibge.gov.br/api/v2/censos/nomes/ranking/?decada=1950');
+        const dataServicoDados = await resServicoDados.json();
+        const { res } = dataServicoDados[0];
+        if (res) {
+            res.map(item => this._listNames.push(item.nome));
+        }
+    }
 };
 export default new GetNames();
