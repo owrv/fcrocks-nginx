f28ea4435797f4facc3987c73c6f7851b21f4806
Wellersom Richardsom Voigt <wellersom.wrv@gmail.com>
2024-12-12 14:01:29 -0300

    base de conexão com o mysql

--- nodejs/src/database/queries.js
+++ nodejs/src/database/queries.js
@@ -1,4 +1,11 @@
 class Queries {
     constructor() {
+        this.connection = mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: 'root',
+            password: 'root',
+            database: 'my_db',
+            insecureAuth: true
+        });
     }
 export default new Queries;
